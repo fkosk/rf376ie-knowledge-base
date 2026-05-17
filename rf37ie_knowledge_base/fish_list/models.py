9756b731f58e096b4f0e3899f34517c55b07ab54
fkosk <wasd3400@gmail.com>
2026-05-17 22:26:28 +0700

    Create Fish model

--- rf37ie_knowledge_base/fish_list/models.py
+++ rf37ie_knowledge_base/fish_list/models.py
@@ -1,3 +1,57 @@
 from django.db import models
 
 # Create your models here.
+class Fish(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=255)
+
+    min_weight = models.IntegerField()
+    max_weight = models.IntegerField()
+
+    min_depth_small = models.IntegerField()
+    min_depth_medium = models.IntegerField()
+    min_depth_large = models.IntegerField()
+
+    max_depth_small = models.IntegerField()
+    max_depth_medium = models.IntegerField()
+    max_depth_large = models.IntegerField()
+
+    favourite_depth_small = models.IntegerField()
+    favourite_depth_medium = models.IntegerField()
+    favourite_depth_large = models.IntegerField()
+
+    filter = models.IntegerField()
+    rating = models.IntegerField()
+    bite_strength = models.IntegerField()
+
+    endurance_small = models.IntegerField()
+    endurance_medium = models.IntegerField()
+    endurance_large = models.IntegerField()
+
+    accuracy_small = models.IntegerField()
+    accuracy_medium = models.IntegerField()
+    accuracy_large = models.IntegerField()
+
+    day_activity_small = models.IntegerField()
+    day_activity_medium = models.IntegerField()
+    day_activity_large = models.IntegerField()
+
+    night_activity_small = models.IntegerField()
+    night_activity_medium = models.IntegerField()
+    night_activity_large = models.IntegerField()
+
+    price = models.IntegerField()
+    rate = models.IntegerField()
+    valuable_weight = models.IntegerField()
+    experience = models.IntegerField()
+    experience_rate = models.IntegerField()
+    biting_direction = models.CharField(max_length=20)
+    chum_base = models.CharField(max_length=20)
+    chum_aromatizer = models.CharField(max_length=20)
+
+    lures_small = models.JSONField()
+    lures_medium = models.JSONField()
+    lures_large = models.JSONField()
+
+    def __str__(self):
+        return self.name
