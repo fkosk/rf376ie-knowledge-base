d5ee19838bcd59d54ef813c2e433b4aac0518770
fkosk <wasd3400@gmail.com>
2026-05-30 20:24:08 +0700

    Basic fish log importer implementation

---
+++ rf37ie_knowledge_base/fishlog_importer/import_fishlog.py
@@ -0,0 +1,17 @@
+from rf37ie_knowledge_base.fishlog_importer.models import FishLog
+
+def import_fish_log(filepath):
+    with filepath.open() as file:
+        for line in file:
+            split_line = line.strip().split(":")
+            fish_log = FishLog(
+                fish_name = split_line[0],
+                fish_weight = split_line[1],
+                bait_name = split_line[2],
+                base_name = split_line[3],
+                location_name = split_line[4],
+                fish_id = split_line[5],
+                time = split_line[6],
+                depth = split_line[7]
+            )
+            fish_log.save()
